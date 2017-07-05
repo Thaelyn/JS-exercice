40990655a0c816ed19af9dd7428e7d6669f1523b
Thaelyn <michael.c.legrand@gmail.com>
2017-07-05 13:58:33 +0200

    Parcourir le tableau

--- array/app.js
+++ array/app.js
@@ -1,16 +1,20 @@
-var arrayFL = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
-var array = document.querySelector("h1");
-array.textContent = "Fruit et Légumes";
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+/*var array = document.querySelector("h1");
+array.textContent = "Fruit et Légumes"; */
 
-var moisArray = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
-var mois = document.querySelector("h2");
+var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
+/*var mois = document.querySelector("h2");
 mois.textContent = "Mois de l'année";
-// var arrayLignes document.getElementById("moisArray").row;
+// var arrayLignes document.getElementById("moisArray").row;*/
 
-alert(moisArray [2]); // Exercice 2
-alert(moisArray [5]); // Exercice 3
-alert(moisArray [10]); // Exercice 4
+alert(mois [2]); // Exercice 2
+alert(mois [5]); // Exercice 3
+alert(mois [10]); // Exercice 4
 
-moisArray [7] = prompt("Modifier la valeur", + "" + moisArray [7]); // Exercice 5
-alert(moisArray [7]); // Excercice 5
+mois [7] = prompt("Modifier la valeur", + "" + mois [7]); // Exercice 5
+alert(mois [7]); // Excercice 5
+
+for ( var i = 0; i < mois.length; i++) { // Exercice 6
+  	alert(mois[i]); // Exercice 6
+}
 
