b974038c52323b73a1240239d51598b7419bd856
Thaelyn <michael.c.legrand@gmail.com>
2017-07-05 12:32:21 +0200

    Exercice 5

--- array/app.js
+++ array/app.js
@@ -1,8 +1,16 @@
-var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var arrayFL = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var array = document.querySelector("h1");
 array.textContent = "Fruit et Légumes";
 
-var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+var moisArray = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
 var mois = document.querySelector("h2");
 mois.textContent = "Mois de l'année";
+// var arrayLignes document.getElementById("moisArray").row;
+
+alert(moisArray [2]); // Exercice 2
+alert(moisArray [5]); // Exercice 3
+alert(moisArray [10]); // Exercice 4
+
+moisArray [7] = prompt("Modifier la valeur", + "" + moisArray [7]); // Exercice 5
+alert(moisArray [7]); // Excercice 5
 
