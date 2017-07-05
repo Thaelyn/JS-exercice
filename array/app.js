d8570ed510d1317608cef04b6637ecf8fa4adecb
Thaelyn <michael.c.legrand@gmail.com>
2017-07-05 11:34:36 +0200

    Création du taleau de l'exercice 1

--- array/app.js
+++ array/app.js
@@ -1,1 +1,8 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var array = document.querySelector("h1");
+array.textContent = "Fruit et Légumes";
+
+var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+var mois = document.querySelector("h2");
+mois.textContent = "Mois de l'année";
+
